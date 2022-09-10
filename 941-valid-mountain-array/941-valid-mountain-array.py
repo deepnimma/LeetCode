1d6a2299c3b9b3a4d0ff97442c0b454fa8c38133
Deepesh Nimma <56356841+NottCurious@users.noreply.github.com>
2022-09-09 20:47:58 -0400

    Time: 230 ms (84.51%), Space: 15.4 MB (67.96%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.py
+++ 941-valid-mountain-array/941-valid-mountain-array.py
@@ -4,46 +4,24 @@ class Solution:
         if len(arr) < 3:
             return False
         
-        # Searching for Max Value
-        highest_val = max(arr)
-        highest_index = arr.index(highest_val)
+        l = 0
+        r = len(arr) - 1
         
-        # Splitting the Array
-        a = arr[:highest_index]
-        b = arr[highest_index:]
-        
-        # Checking if any arrays are empty
-        if a == [] or b == [] or (len(b) == 1 and len(a) != 1):
-            return False
-        
-        # Sorting the arrays
-        sorted_a = sorted(a)
-        sorted_b = sorted(b, reverse=True)
-        
-        print(a)
-        print(sorted_a)
-        print(b)
-        print(sorted_b)
-        
-        # Checking for duplicates
-        for i, val in enumerate(sorted_a[:-1]):
-            if sorted_a[i + 1] == val:
-                return False
-        
-        for i, val in enumerate(sorted_b[:-1]):
-            if sorted_b[i + 1] == val:
+        while(arr[l] < arr[l + 1]):
+            l += 1
+            
+            # Is the final element
+            if(l == len(arr) - 1):
                 return False
             
-        # Comparing the Arrays
-        if sorted_a == a and sorted_b == b:
+        while(arr[r] < arr[r - 1]):
+            r -= 1
+            
+            # Is the first element
+            if(r == 0):
+                return False
+            
+        if(l == r):
             return True
-        else:
-            return False
-    
-if __name__ == '__main__':
-    print(Solution().validMountainArray([0, 3, 2, 1]))
-   # print(Solution().validMountainArray([0,3,2,1]))
-   # print(Solution().validMountainArray([0, 0, 0]))
-   # print(Solution().validMountainArray([1, 2, 3, 4, 5, 6]))
-   # print(Solution().validMountainArray([0, 3, 1]))
-    
+        
+        return False
