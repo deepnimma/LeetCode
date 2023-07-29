079af3fc6f30e87615ae2f3863baf2c531cfe993
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2023-07-29 00:09:05 -0400

    Time: 2 ms (91.71%), Space: 40.4 MB (91.44%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> myStack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (myStack.size() == 0 || "({[".indexOf(c) != -1)
+                myStack.push(c);
+            else {
+                if(!checkBracket(myStack.pop(), c))
+                   return false;
+            }  // if-else
+        } // FOR
+
+        if (myStack.size() != 0)
+            return false;
+
+        return true;
+    } // isValid
+
+    private boolean checkBracket(char a, char b) {
+        if ((a == '(' && b == ')') ||
+        (a == '[' && b == ']') ||
+        (a == '{' && b == '}'))
+            return true;
+        return false;
+    } // checkBracket
+} // Solution
