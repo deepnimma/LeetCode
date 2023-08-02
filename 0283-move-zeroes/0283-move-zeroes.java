2af9704241385449129e97260f1327a974275071
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2023-08-02 00:49:29 -0400

    Time: 2 ms (41.78%), Space: 45.3 MB (26.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = 0; j < nums.length - i - 1; j++) {
-                if (nums[j] == 0) {
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = 0;
-                } // if
-            } // for
+        for (int i = 0, j = 0; i < nums.length;) {
+            if (nums[i] == 0) i++;
+            else {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++; j++;
+            } // else
         } // for
     } // moveZeres
 } // Solution
