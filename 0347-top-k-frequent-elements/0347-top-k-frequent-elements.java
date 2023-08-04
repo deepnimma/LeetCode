ff6a3b9801b0fb3231acb889d310f5dafc11913e
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2023-08-03 23:59:33 -0400

    Time: 45 ms (8.89%), Space: 47.7 MB (47.27%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        } // for
+
+        int[] a = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            a[i] = getMostFrequent(map);
+            map.replace(a[i], -1);
+        } // for
+
+        System.out.println(map.values());
+     
+        return a;
+    } // topKFrequent
+
+    int getMostFrequent(Map<Integer, Integer> map) {
+        int mostFrequent = Integer.MIN_VALUE;
+        int freq = Integer.MIN_VALUE;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > freq) {
+                mostFrequent = entry.getKey();
+                freq = entry.getValue();
+            } // if
+        } // for
+
+        return mostFrequent;
+    }
+} // Solution
