145d95bfbdadfc48d1670798e1efa2f61059b142
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2025-03-16 18:50:17 -0400

    Time: 3 ms (90.16%), Space: 44.4 MB (56.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,17 @@
-import java.util.Hashtable;
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int sl = s.length();
-        int tl = t.length();
+        char[] cs = s.toCharArray();
+        char[] ct = t.toCharArray();
 
-        if (sl != tl) return false;
+        if (cs.length != ct.length) return false;
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        Arrays.sort(ct);
+        Arrays.sort(cs);
 
-        for (int i = 0; i < sl; i++) {
-            Character sc = s.charAt(i);
-            Character tc = t.charAt(i);
-
-            map.put(sc, map.getOrDefault(sc, 0) + 1);
-            map.put(tc, map.getOrDefault(tc, 0) - 1);
+        for (int i = 0; i < ct.length; i++) {
+            if (cs[i] != ct[i]) return false;
         } // for
 
-        for (int i : map.values())
-            if ( i != 0 )
-                return false;
-        
         return true;
     } // isAnagram
 } // Solution
